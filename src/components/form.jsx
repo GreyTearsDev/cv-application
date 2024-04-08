6947ad7e25505482e0b4ac319b0f794785ea75d6
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-08 18:13:04 +0100

    feat: Create 'FormGeneralInformation' component

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -12,3 +12,18 @@ function InputField({ labelInfo, type, onChange }) {
 InputField.defaultProps = {
   type: "text",
 };
+
+function FormGeneralInformation({ onChange }) {
+  return (
+    <fieldSet>
+      <legend>General Information</legend>
+      <InputField labelInfo={"Name"} onChange={onChange} />
+      <InputField labelInfo={"Email"} type={"email"} onChange={onChange} />
+      <InputField labelInfo={"Phone Number"} type={"tel"} onChange={onChange} />
+    </fieldSet>
+  );
+}
+
+export function Form() {
+  return <FormGeneralInformation onChange={() => {}} />;
+}
