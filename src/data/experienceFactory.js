7be95bcf2b667f8426072d4f7c08c1f06d01619f
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-09 14:11:28 +0100

    refactor: Refactor 'ExperienceFactory.js' to generate unique id using uui

--- src/data/experienceFactory.js
+++ src/data/experienceFactory.js
@@ -1,5 +1,8 @@
+import {v4 as uuid} from 'uuid';
+
 export default function ExperienceFactory(companyName, role, responsabilities, dateStart, dateEnd) {
   return {
+    id: uuid(),
     company: companyName,
     roleTitle: role,
     mainResponsabilities: responsabilities,
