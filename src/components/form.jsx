c111e11871fb9811fa0f4f0806158366f8fee036
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-09 09:51:01 +0100

    refactor: Refactor 'FormGeneralInformation' to handle state updates directly

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,8 +1,8 @@
 function InputField({ labelName, labelFor, type, onChange }) {
   return (
     <>
-      <label for={labelFor}>
-        {labelName + " :"}
+      <label htmlFor={labelFor}>
+        {labelName + ": "}
         <input type={type} id={labelFor} onChange={onChange} />
       </label>
     </>
@@ -13,30 +13,34 @@ InputField.defaultProps = {
   type: "text",
 };
 
-function FormGeneralInformation({ onChange }) {
+function FormGeneralInformation({ userData, setUserData }) {
+  function handleChange(e, fieldName) {
+    setUserData({ ...userData, [fieldName]: e.target.value });
+    console.log(userData);
+  }
   return (
     <>
-      <InputField labelName={"First Name"} labelFor={"first-name"} onChange={onChange} />
-      <InputField labelName={"Last Name"} labelFor={"last-name"} onChange={onChange} />
-      <InputField labelName={"Email"} type={"email"} labelFor={"email"} onChange={onChange} />
-      <InputField labelName={"Phone"} labelFor={"email"} type={"tel"} onChange={onChange} />
+      <InputField labelName={"First Name"} labelFor={"first-name"} onChange={(e) => handleChange(e, "firstName")} />
+      <InputField labelName={"Last Name"} labelFor={"last-name"} onChange={(e) => handleChange(e, "lastName")} />
+      <InputField labelName={"Email"} type={"email"} labelFor={"email"} onChange={(e) => handleChange(e, "email")} />
+      <InputField labelName={"Phone"} labelFor={"email"} type={"tel"} onChange={(e) => handleChange(e, "phone")} />
     </>
   );
 }
 
 function Section({ name, children }) {
   return (
-    <fieldSet>
+    <fieldset>
       <legend>{name}</legend>
       {children}
-    </fieldSet>
+    </fieldset>
   );
 }
 
-export function Form() {
+export function Form({ userData, setUserData }) {
   return (
     <Section name={"General Information"}>
-      <FormGeneralInformation onChange={() => {}} />
+      <FormGeneralInformation userData={userData} setUserData={setUserData} onChange={() => {}} />
     </Section>
   );
 }
