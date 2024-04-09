0420faa6d22618839f914ee5b1724ae3cab29a53
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-09 15:15:43 +0100

    refactor: Refactor 'FormGeneralInformation' and 'FormEducationalBackground' to use 'user' object directly

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,3 +1,10 @@
+import { useState } from "react";
+import SchoolFactory from "../data/schoolFactory";
+
+function Button({ type, onClick, text }) {
+  return <button type={type} onClick={onClick}>{text}</button>;
+}
+
 function InputField({ labelName, labelFor, type, onChange }) {
   return (
     <>
@@ -13,17 +20,66 @@ InputField.defaultProps = {
   type: "text",
 };
 
-function FormGeneralInformation({ userData, setUserData }) {
+function FormGeneralInformation({ user }) {
   function handleChange(e, fieldName) {
-    setUserData({ ...userData, [fieldName]: e.target.value });
-    console.log(userData);
+    user[fieldName] = e.target.value;
   }
+
   return (
     <>
       <InputField labelName={"First Name"} labelFor={"first-name"} onChange={(e) => handleChange(e, "firstName")} />
       <InputField labelName={"Last Name"} labelFor={"last-name"} onChange={(e) => handleChange(e, "lastName")} />
       <InputField labelName={"Email"} type={"email"} labelFor={"email"} onChange={(e) => handleChange(e, "email")} />
       <InputField labelName={"Phone"} labelFor={"email"} type={"tel"} onChange={(e) => handleChange(e, "phone")} />
+    </>
+  );
+}
+
+function FormEducationalBackground({ user }) {
+  const [schoolInfo, setSchoolInfo] = useState(SchoolFactory());
+
+  function handleAddClick() {
+    user.schools.push(schoolInfo);
+    setSchoolInfo(SchoolFactory());
+  }
+
+  function handleChange(e, fieldName) {
+    setSchoolInfo({ ...schoolInfo, [fieldName]: e.target.value });
+  }
+
+  return (
+    <>
+      <InputField
+        labelName={"School Name"}
+        LabelFor={"school-name"}
+        onChange={(e) => {
+          handleChange(e, "name");
+        }}
+      />
+      <InputField
+        labelName={"Field of Study"}
+        LabelFor={"field-of-study"}
+        onChange={(e) => {
+          handleChange(e, "field");
+        }}
+      />
+      <InputField
+        labelName={"From"}
+        LabelFor={"from"}
+        type={"date"}
+        onChange={(e) => {
+          handleChange(e, "start");
+        }}
+      />
+      <InputField
+        labelName={"To"}
+        LabelFor={"to"}
+        type={"date"}
+        onChange={(e) => {
+          handleChange(e, "end");
+        }}
+      />
+      <Button type="button" text="Add" onClick={handleAddClick} />
     </>
   );
 }
@@ -37,13 +93,15 @@ function Section({ name, children }) {
   );
 }
 
-export function Form({ userData, setUserData }) {
+export function Form({ user }) {
   return (
     <form>
       <Section name={"General Information"}>
-        <FormGeneralInformation userData={userData} setUserData={setUserData} onChange={() => {}} />
+        <FormGeneralInformation user={user} />
       </Section>
-      <Section name={"Educational Background"} />
+      <Section name={"Educational Background"}>
+        <FormEducationalBackground user={user} />
+      </Section>
       <Section name={"Job Experience"} />
     </form>
   );
