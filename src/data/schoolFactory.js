ee7baa2dfa97580707c7309f3c6106f0f5e39ebf
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-09 14:09:33 +0100

    refactor: Refactor 'SchoolFactory.js' to generate a unique ID using uuid

--- src/data/schoolFactory.js
+++ src/data/schoolFactory.js
@@ -1,5 +1,8 @@
+import {v4 as uuid} from 'uuid';
+
 export default function SchoolFactory(schoolName, fieldOfStudy, dateStart, dateEnd) {
   return {
+    id: uuid(),
     name: schoolName,
     field: fieldOfStudy,
     start: dateStart,
