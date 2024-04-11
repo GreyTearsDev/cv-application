98b5dab943b2acf1dd60dfbebfee710d9f08f2d0
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-11 15:43:32 +0100

    refactor: Add 'MainPage' component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import { useState } from "react";
-import { Form } from "./components/form";
+import Form from "./components/form";
+import MainPage from "./components/main-page";
 import { User } from "./data/user";
 
 function App() {
   const [user, setUser] = useState(User);
-  return <Form user={user} setUser={setUser} />;
+  return (
+    <>
+      <Form user={user} setUser={setUser} />
+      <MainPage user={user} setUser={setUser} />
+    </>
+  );
 }
 
 export default App;
