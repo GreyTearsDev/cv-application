663660b993e86f85ac8c6d994cb615808a3ab15b
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-11 17:40:58 +0100

    feat: Add reset functionality to input fields in 'FormEducationalBackground' component
    added logic to reset input fields in the 'FormEducationalBackground' component when the form is submitted

--- src/components/form-educational-background.jsx
+++ src/components/form-educational-background.jsx
@@ -6,10 +6,14 @@ import InputField from "./input-field";
 
 export default function FormEducationalBackground({ setUser }) {
   const [schoolInfo, setSchoolInfo] = useState(SchoolFactory());
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [resetKey, setResetKey] = useState(0);
 
   function handleAddClick() {
     setUser(prevState => ({ ...prevState, schools: [...prevState.schools, schoolInfo] }));
+    setIsSubmitted(true);
     setSchoolInfo(SchoolFactory());
+    setResetKey(prevKey => prevKey + 1);
   }
 
   function handleChange(e, fieldName) {
@@ -20,36 +24,48 @@ export default function FormEducationalBackground({ setUser }) {
     <>
       <div>
         <InputField
+          key={`school-name${resetKey}`}
           requiredField={true}
           labelName="School Name"
           labelFor="school-name"
           onChange={(e) => {
             handleChange(e, "name");
           }}
+          isSubmitted={isSubmitted}
+          setIsSubmitted={setIsSubmitted}
         />
         <InputField
+          key={`field-of-study${resetKey}`}
           requiredField={true}
           labelName="Field of Study"
           labelFor="field-of-study"
           onChange={(e) => {
             handleChange(e, "field");
           }}
+          isSubmitted={isSubmitted}
+          setIsSubmitted={setIsSubmitted}
         />
         <InputField
+          key={`from${resetKey}`}
           labelName="From"
           labelFor="from"
           type="date"
           onChange={(e) => {
             handleChange(e, "start");
           }}
+          isSubmitted={isSubmitted}
+          setIsSubmitted={setIsSubmitted}
         />
         <InputField
+          key={`to${resetKey}`}
           labelName="To"
           labelFor="to"
           type="date"
           onChange={(e) => {
             handleChange(e, "end");
           }}
+          isSubmitted={isSubmitted}
+          setIsSubmitted={setIsSubmitted}
         />
       </div>
       <Button type="button" text="Add" onClick={handleAddClick} />
