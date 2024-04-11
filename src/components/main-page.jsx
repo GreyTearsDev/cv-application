cd78d8c852b3242e56836d1e65a9232a8bf06836
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-11 18:12:16 +0100

    refactor: Import 'Header' component and display it on the main page

--- src/components/main-page.jsx
+++ src/components/main-page.jsx
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
 import DynamicDisplaySection from "./dynamic-display-section";
+import Header from "./header";
 
 export default function MainPage({ user, setUser }) {
   return (
     <>
+      <Header user={user} />
       <DynamicDisplaySection arrayName={"schools"} user={user} setUser={setUser} />
       <DynamicDisplaySection arrayName={"experience"} user={user} setUser={setUser} />
     </>
