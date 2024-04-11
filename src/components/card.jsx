33c943befe1963d3bbec8f2e7745762c489d5c56
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-11 14:22:52 +0100

    feat: Define 'Card' component

---
+++ src/components/card.jsx
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types";
+import Button from "./button";
+
+export default function Card({ name, roleOrField, id, dateStart, dateEnd, onClick, children }) {
+  return (
+    <div className="card">
+      <Button
+        onClick={() => {
+          onClick(id);
+        }}
+        text="x"
+      />
+      <h3>{name}</h3>
+      <h4>{roleOrField}</h4>
+      <div className="card__date">
+        <p>
+          From: <span>{dateStart}</span>
+        </p>
+        <p>
+          To: <span>{dateEnd}</span>
+        </p>
+      </div>
+      <p>{children}</p>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  name: PropTypes.string,
+  roleOrField: PropTypes.string,
+  id: PropTypes.number,
+  dateStart: PropTypes.string,
+  dateEnd: PropTypes.string,
+  onClick: PropTypes.func,
+  children: PropTypes.array,
+};
