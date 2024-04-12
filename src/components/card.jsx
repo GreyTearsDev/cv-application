ac3e700bb1b28d18c2a1d97e8727cb6d007310fb
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-12 11:22:36 +0100

    refactor: Refactor 'Card' component to use svg for delete icon.

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,15 +1,19 @@
 import PropTypes from "prop-types";
-import Button from "./button";
 
 export default function Card({ name, roleOrField, id, dateStart, dateEnd, onClick, text }) {
   return (
     <div className="card">
-      <Button
+      <svg
+        className="icon"
+        xmlns="http://www.w3.org/2000/svg"
         onClick={() => {
           onClick(id);
         }}
-        text="x"
-      />
+        viewBox="0 0 24 24"
+      >
+        <title>trash-can-outline</title>
+        <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
+      </svg>{" "}
       <h3>{name}</h3>
       <h4>{roleOrField}</h4>
       <div className="card__date">
