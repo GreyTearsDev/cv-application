b95f2e9b8c74b60d164f64d44a935a8cad0e8017
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-12 11:43:47 +0100

    refactor: Refactor components to use SVG for add icon and delete 'button.jsx' as it's no longer used

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -13,7 +13,7 @@ export default function Card({ name, roleOrField, id, dateStart, dateEnd, onClic
       >
         <title>trash-can-outline</title>
         <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
-      </svg>{" "}
+      </svg>
       <h3>{name}</h3>
       <h4>{roleOrField}</h4>
       <div className="card__date">
