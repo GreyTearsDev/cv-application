99403bde5db5b97ff1cc0874483f21c638b76f9e
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-13 15:44:43 +0100

    feat: Add download PDF functionality
    - added html2canvas and jsPDF dependencies for generating PDFs from the main section
    - added a button to download the PDF in 'main-page.jsx'
    - styled the download button in main.css

--- src/components/main-page.jsx
+++ src/components/main-page.jsx
@@ -1,14 +1,45 @@
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 import PropTypes from "prop-types";
+import { useRef } from "react";
 import DynamicDisplaySection from "./dynamic-display-section";
 import Header from "./header";
 
 export default function MainPage({ user, setUser }) {
+  const pdfRef = useRef();
+
+  const downloadPDF = () => {
+    const input = pdfRef.current;
+
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a4", true);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const imgWidth = canvas.width;
+      const imgHeight = canvas.height;
+      const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+      const imgX = (pdfWidth - imgWidth * ratio) / 2;
+      const imgY = 30;
+
+      pdf.addImage(imgData, "PNG", imgX, imgY, imgWidth * ratio, imgHeight * ratio);
+      pdf.save(`${user.firstName.toLowerCase()}-${user.lastName.toLowerCase()}'s - cv.pdf`);
+    });
+  };
+
   return (
-    <div className="main-section">
-      <Header user={user} />
-      <DynamicDisplaySection arrayName={"schools"} user={user} setUser={setUser} />
-      <DynamicDisplaySection arrayName={"experience"} user={user} setUser={setUser} />
-    </div>
+    <>
+      <button className="btn__download" onClick={downloadPDF}>
+        DOWNLOAD PDF
+      </button>
+      <div ref={pdfRef}>
+        <div className="main-section">
+          <Header user={user} />
+          <DynamicDisplaySection arrayName={"schools"} user={user} setUser={setUser} />
+          <DynamicDisplaySection arrayName={"experience"} user={user} setUser={setUser} />
+        </div>
+      </div>
+    </>
   );
 }
 
