c13a27af84ac3f0748ff327f0dd1c248f116f970
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-13 14:08:42 +0100

    refactor: Update class name in main-page.jsx

--- src/components/main-page.jsx
+++ src/components/main-page.jsx
@@ -4,7 +4,7 @@ import Header from "./header";
 
 export default function MainPage({ user, setUser }) {
   return (
-    <div>
+    <div className="main-section">
       <Header user={user} />
       <DynamicDisplaySection arrayName={"schools"} user={user} setUser={setUser} />
       <DynamicDisplaySection arrayName={"experience"} user={user} setUser={setUser} />
