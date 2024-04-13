fdefb47f727f5f522af30cafe48a059ff5364f93
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-13 07:24:24 +0100

    feat: Add toggle functionality to section visibility
    implemented onClick handler in Section component to toggle visibility of its children when clicked. This  allows users to expand and collapse sections in the form

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -5,16 +5,20 @@ import FormJobExperience from "./form-job-experience";
 import Section from "./section";
 
 export default function Form({ user, setUser }) {
+  const toggleVisibility = (visibilityValue, setVisibility) => {
+    setVisibility(!visibilityValue);
+  };
+
   return (
     <form>
       <p>Required fields are marked with an asterisk (*)</p>
-      <Section name={"General Information"}>
+      <Section onClick={toggleVisibility} name={"General Information"}>
         <FormGeneralInformation user={user} setUser={setUser} />
       </Section>
-      <Section name={"Educational Background"}>
+      <Section onClick={toggleVisibility} name={"Educational Background"}>
         <FormEducationalBackground setUser={setUser} />
       </Section>
-      <Section name={"Job Experience"}>
+      <Section onClick={toggleVisibility} name={"Job Experience"}>
         <FormJobExperience setUser={setUser} />
       </Section>
     </form>
--- src/components/section.jsx
+++ src/components/section.jsx
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
-export default function Section({ name, children }) {
+export default function Section({ name, children, onClick }) {
+  const [showChildren, setShowChildren] = useState(false);
+
   return (
-    <fieldset>
+    <fieldset onClick={() => onClick(showChildren, setShowChildren)}>
       <legend>{name}</legend>
-      {children}
+      {showChildren ? children : ""}
     </fieldset>
   );
 }
@@ -12,4 +15,5 @@ export default function Section({ name, children }) {
 Section.propTypes = {
   name: PropTypes.string,
   children: PropTypes.object,
+  onClick: PropTypes.func,
 };
