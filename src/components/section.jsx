fe6b9c367d000a2d21b33d8c7ffc7be5f468b6fd
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-13 07:30:41 +0100

    style: Add arrow icons to indicate section expansion

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -6,7 +6,22 @@ export default function Section({ name, children, onClick }) {
 
   return (
     <fieldset onClick={() => onClick(showChildren, setShowChildren)}>
-      <legend>{name}</legend>
+      <legend>
+        {name}
+        {showChildren
+          ? (
+            <svg className="icon icon--arrow icon--arrow-up" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <title>chevron-up</title>
+              <path d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z" />
+            </svg>
+          )
+          : (
+            <svg className="icon icon--arrow icon--arrow--down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <title>chevron-down</title>
+              <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+            </svg>
+          )}
+      </legend>
       {showChildren ? children : ""}
     </fieldset>
   );
