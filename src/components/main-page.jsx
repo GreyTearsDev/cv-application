ef647601bf20435397a78ba829fc473659a0a583
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-14 21:30:10 +0100

    fix: Hide delete icons during processing of PDF download
    adjusted the PDF generation process to hide delete icons during processing using a 1-second timeout, stopping the delete icons from appearing in the generated PDF

--- src/components/main-page.jsx
+++ src/components/main-page.jsx
@@ -13,20 +13,22 @@ export default function MainPage({ user, setUser }) {
     const input = pdfRef.current;
     setHideDeleteIcons(true);
 
-    html2canvas(input).then((canvas) => {
-      const imgData = canvas.toDataURL("image/png");
-      const pdf = new jsPDF("p", "mm", "a4", true);
-      const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight = pdf.internal.pageSize.getHeight();
-      const imgWidth = canvas.width;
-      const imgHeight = canvas.height;
-      const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
-      const imgX = (pdfWidth - imgWidth * ratio) / 2;
-      const imgY = 10;
+    setTimeout(() => {
+      html2canvas(input).then((canvas) => {
+        const imgData = canvas.toDataURL("image/png");
+        const pdf = new jsPDF("p", "mm", "a4", true);
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = pdf.internal.pageSize.getHeight();
+        const imgWidth = canvas.width;
+        const imgHeight = canvas.height;
+        const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+        const imgX = (pdfWidth - imgWidth * ratio) / 2;
+        const imgY = 10;
 
-      pdf.addImage(imgData, "PNG", imgX, imgY, imgWidth * ratio, imgHeight * ratio);
-      pdf.save(`${user.firstName} ${user.lastName} CV.pdf`);
-    }).then(() => setHideDeleteIcons(false));
+        pdf.addImage(imgData, "PNG", imgX, imgY, imgWidth * ratio, imgHeight * ratio);
+        pdf.save(`${user.firstName} ${user.lastName} CV.pdf`);
+      }).then(() => setHideDeleteIcons(false));
+    }, 1000);
   };
 
   return (
