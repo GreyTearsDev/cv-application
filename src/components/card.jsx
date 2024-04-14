273ed976e58ffc7d517e1085a7a99e660e1e2067
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-14 10:53:30 +0100

    refactor: Replace svg acting as delete button by an actual button element

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -11,18 +11,7 @@ export default function Card({ name, roleOrField, id, dateStart, dateEnd, onClic
   return (
     <div className="card">
       {!hideIcons
-        ? (
-          <svg
-            className="icon icon--button"
-            xmlns="http://www.w3.org/2000/svg"
-            onClick={() => {
-              onClick(id);
-            }}
-            viewBox="0 0 24 24"
-          >
-            <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
-          </svg>
-        )
+        ? <button className="btn btn__delete" onClick={() => onClick(id)}>x</button>
         : ""}
       <h3>{name}</h3>
       <h4>{roleOrField}</h4>
